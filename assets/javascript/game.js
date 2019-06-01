bc3b98bdd429594e57893aecba913619e531fcc0
Kevin McGregor <Kevin.McGregor@edgenuity.com>
2019-05-31 20:21:15 -0700

    Lowering guesses and reseting guesses remaining complete

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -4,7 +4,13 @@ let guessesLeft = 9;
 let guess;
 
 function updateGuess(){
-    const targetDiv = document.getElementById("currentGuesses");
-    guessesLeft--
+    const targetDiv = document.getElementById("guessesLeft");
+    guessesLeft--;
+    targetDiv.textContent = guessesLeft;
+}
+
+function resetGuess(){
+    guessesLeft = 9;
+    const targetDiv = document.getElementById("guessesLeft");
     targetDiv.textContent = guessesLeft;
 }
