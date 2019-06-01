b3f77514223ead47db67f005b737b1d990e8e5a5
Kevin McGregor <Kevin.McGregor@edgenuity.com>
2019-06-01 15:46:44 -0700

    program functional

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,15 +2,51 @@ let win = 0;
 let loss = 0;
 let guessesLeft = 9;
 let guess;
+const letterArray = ["a", "b", "c", "d", "e", "f", "g", 
+    "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", 
+    "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+
+function updateLetter(){
+    letter = letterArray[Math.floor(Math.random() * 26)];
+}
 
 function updateGuess(){
     const targetDiv = document.getElementById("guessesLeft");
     guessesLeft--;
     targetDiv.textContent = guessesLeft;
+    guessList();
 }
 
 function resetGuess(){
     guessesLeft = 9;
-    const targetDiv = document.getElementById("guessesLeft");
+    let targetDiv = document.getElementById("guessesLeft");
     targetDiv.textContent = guessesLeft;
+    targetDiv = document.getElementById("currentGuesses");
+    targetDiv.textContent = "";
+}
+
+function winFunc(){
+    alert("Correct!");
+    resetGuess();
+    updateLetter();
+    win++;
+    const targetDiv = document.getElementById("wins");
+    targetDiv.textContent = win;
+}
+
+function lossFunc(){
+    resetGuess();
+    updateLetter();
+    loss++;
+    const targetDiv = document.getElementById("losses");
+    targetDiv.textContent = loss;
+    alert("Out of guesses!");
+}
+
+function guessList(){
+    const targetDiv = document.getElementById("currentGuesses");
+    targetDiv.textContent = targetDiv.textContent + " " + event.key;
+    // const newDiv = document.createElement("div");
+    // newDiv.textContent = event.key;
+    // targetDiv.appendChild(newDiv);
 }
